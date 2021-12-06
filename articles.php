bd403d227b974cb7b0c2bd0d5f27296b297a3d20
dorian-palace <dorian.palace@laplateforme.io>
2021-12-06 14:40:39 +0100

    articles

--- articles.php
+++ articles.php
@@ -7,29 +7,10 @@ include('elements/bdd.php');
 //$articles = $req->execute();
 ?>
 
-<div class="articles_5">
-    <?php /*
-    $limite = 5;
-
-    $req = $bdd->prepare("SELECT * FROM articles LIMIT :limite ");
-    $req->bindValue('limite', $limite, PDO::PARAM_INT);
-    // Le marqueur est nommé « limite » il doit prendre la valeur de la variable $limite cette valeur est de type entier
-    $req->execute();
-
-    //while ($articles = $req->fetch()) {
-    //echo $articles['nom'] . $articles['article'] . '<br/>' . '<br/>';
-    //}
-
-    while ($element = $req->fetch()) {
-        echo $element['article'] . '<br/>';
-    } */
-    ?>
-</div>
-
-<h1>Article</h1>
-
+<h2>articles / 5</h2>
 <div class="pagination_suivant">
     <?php
+    //articles par 5 + pagination
     $page = (!empty($_GET['page']) ? $_GET['page'] : 1);
     $limite = 5;
 
@@ -40,44 +21,105 @@ include('elements/bdd.php');
     $req->execute();
 
     while ($elements = $req->fetch()) {
-        echo  $elements['login']. ' '. ':'.$elements['article'].   '<br />';
+        echo  $elements['login'] . ' ' . ':' . $elements['article'] . ' ' . $elements['date'] .   '<br />';
     }
     ?>
     <?php
 
-$resultFoundRows = $bdd->query('SELECT count(id) FROM articles');
-/* On doit extraire le nombre du jeu de résultat */
-$nombredElementsTotal = $resultFoundRows->fetchColumn();
+    $result = $bdd->query('SELECT count(id) FROM articles');
+    $nbelements = $result->fetchColumn();
 
-$debut = ($page - 1) * $limite;
-/* Cette requête ne change pas */
-$req = 'SELECT * FROM articles LIMIT :limite OFFSET :debut';
-$req = $bdd->prepare($req);
+    $debut = ($page - 1) * $limite;
+    $req = 'SELECT * FROM articles LIMIT :limite OFFSET :debut';
+    $req = $bdd->prepare($req);
+    $req->bindValue('debut', $debut, PDO::PARAM_INT);
+    $req->bindValue('limite', $limite, PDO::PARAM_INT);
+    $req->execute();
+
+    $nbpage = ceil($nbelements / $limite);
+
+
+    if ($page > 1) :
+    ?><a href="?page=<?php echo $page - 1; ?>">Page précédente</a> — <?php
+    endif;
+
+    for ($i = 1; $i <= $nbpage; $i++) :
+    ?><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
+    endfor;
+
+
+    if ($page < $nbpage) :
+    ?>— <a href="?page=<?php echo $page + 1; ?>">Page suivante</a><?php
+    endif;
+    ?>
+
+    <br /> <br />
+    <br />
+    <br />
+
+    <h2>categorie / 5</h2>
+    <?php
+    //Articles par catégorie 
+
+    //categorie par 5 + pagination
+    $page_categorie = (!empty($_GET['categorie']) ? $_GET['categorie'] : 1);
+    $limite = 5;
+
+    $debut = ($page_categorie - 1) * $limite;
+    $req = $bdd->prepare("SELECT * FROM articles INNER JOIN utilisateurs ON utilisateurs.id = articles.id_utilisateur INNER JOIN categories ON articles.id_categorie = categories.id  ORDER BY  date DESC LIMIT 5 ");
+    $req->bindValue('debut', $debut, PDO::PARAM_INT);
+    $req->bindValue('limite', $limite, PDO::PARAM_INT);
+    $req->execute();
+
+    while ($elements = $req->fetch()) {
+        echo  $elements['login'] . ' ' . ':' . $elements['article'] . ' ' . $elements['date'] . ' ' . $elements['id_categorie'] .  ' ' . $elements['nom'] .  '<br />';
+    }
+
+
+    $result = $bdd->query('SELECT count(id) FROM categories');
+    $nbelements = $result->fetchColumn();
+
+    $debut = ($page - 1) * $limite;
+    $req = 'SELECT * FROM categories LIMIT :limite OFFSET :debut';
+    $req = $bdd->prepare($req);
+    $req->bindValue('debut', $debut, PDO::PARAM_INT);
+    $req->bindValue('limite', $limite, PDO::PARAM_INT);
+    $req->execute();
+
+    $nbpage = ceil($nbelements / $limite); //ceil — Arrondit au nombre supérieur
+
+
+    if ($page > 1) :
+    ?><a href="?categorie=<?php echo $page - 1; ?>">Page précédente</a> — <?php
+    endif;
+
+    for ($i = 1; $i <= $nbpage; $i++) :
+    ?><a href="?categorie=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
+    endfor;
+
+
+    if ($page < $nbpage) :
+    ?>— <a href="?categorie=<?php echo $page + 1; ?>">Page suivante</a><?php
+    endif;
+    ?>
+<br /> <br /> <br />
+<?php
+//Link / catégories <a>
+$page_categorie = (!empty($_GET['categorie']) ? $_GET['categorie'] : 1);
+$limite = 5;
+
+$debut = ($page_categorie - 1) * $limite;
+$req=$bdd->query('SELECT * FROM categories ORDER BY nom');
 $req->bindValue('debut', $debut, PDO::PARAM_INT);
 $req->bindValue('limite', $limite, PDO::PARAM_INT);
 $req->execute();
 
-$nombreDePages = ceil($nombredElementsTotal / $limite);
+while ($elements = $req->fetch()) {
+    echo  $elements['id'] . ' ' . ':' . $elements['nom'] .  '<br />';
+}
 
-/* Si on est sur la première page, on n'a pas besoin d'afficher de lien
- * vers la précédente. On va donc ne l'afficher que si on est sur une autre
- * page que la première */
-if ($page > 1):
-    ?><a href="?page=<?php echo $page - 1; ?>">Page précédente</a> — <?php
-endif;
-
-/* On va effectuer une boucle autant de fois que l'on a de pages */
-for ($i = 1; $i <= $nombreDePages; $i++):
-    ?><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
-endfor;
-
-/* Avec le nombre total de pages, on peut aussi masquer le lien
- * vers la page suivante quand on est sur la dernière */
-if ($page < $nombreDePages):
-    ?>— <a href="?page=<?php echo $page + 1; ?>">Page suivante</a><?php
-endif;
 ?>
-
+</div>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -89,7 +131,6 @@ endif;
 </head>
 
 <body>
-
 </body>
 
 </html>
