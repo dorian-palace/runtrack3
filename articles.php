acf7e0e190c5028c16df25f05868432d26cb4174
tony-guillot <tony.guilot@laplateforme.io>
2021-12-08 13:17:43 +0100

    categorie articles

--- articles.php
+++ articles.php
@@ -19,89 +19,91 @@ $req_article->bindValue('debut', $debut, PDO::PARAM_INT);
 $req_article->bindValue('limite', $limite, PDO::PARAM_INT);
 $req_article->execute();
 
+$result = $bdd->query('SELECT count(id) FROM articles');
+$nbelements = $result->fetchColumn();
 
+$debut = ($page - 1) * $limite;
+$req = 'SELECT * FROM articles LIMIT :limite OFFSET :debut';
+$req = $bdd->prepare($req);
+$req->bindValue('debut', $debut, PDO::PARAM_INT);
+$req->bindValue('limite', $limite, PDO::PARAM_INT);
+$req->execute();
+
+if (isset($_GET['categorie']) and !empty($_GET['categorie'])) {
+
+    $get_categorie = $_GET['categorie'];
+    $req = $bdd->prepare("SELECT * FROM articles INNER JOIN categories ON categories.id = articles.id_categorie where id_categorie = ?");
+    $cat1 = $req->execute(array($get_categorie));
+    
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Articles</title>
-</head>
-
-<body>
-    <?php include('elements/header.php'); ?>
-
-
-    <div class="articles">
-        <?php
-        while ($elements =  $req_article->fetch()) { ?>
-            <h4 id="h4articles"><?php echo  $elements['login'] . ' ' . ':' . $elements['article'] . ' ' . $elements['date']; ?> </h4> <br />
-        <?php } ?>
-    </div>
-
-
-
-    <?php
-
-    $result = $bdd->query('SELECT count(id) FROM articles');
-    $nbelements = $result->fetchColumn();
-
-    $debut = ($page - 1) * $limite;
-    $req = 'SELECT * FROM articles LIMIT :limite OFFSET :debut';
-    $req = $bdd->prepare($req);
-    $req->bindValue('debut', $debut, PDO::PARAM_INT);
-    $req->bindValue('limite', $limite, PDO::PARAM_INT);
-    $req->execute();
-
-    //arrondi à l'entier le plus proche, permert d'avoir touchoir un entier
-    $nbpage = ceil($nbelements / $limite); ?>
-
-    <?php
-    
-    if (isset($_GET['categorie']) and !empty($_GET['categorie'])) {
-
-        $get_categorie = $_GET['categorie'];
-        $req = $bdd->prepare("SELECT * FROM articles INNER JOIN categories ON categories.id = articles.id_categorie where id_categorie = ?");
-        $cat1=$req->execute(array($get_categorie));
-        $categorie_elements = $req->fetch();
-
-       
-  
-       
-    ?>
     <div class="categories">
-    
-    <?php 
 
-        if ($categorie_elements['id_categorie'] == 1){
-            echo $categorie_elements['article'];
-        }
+    <?php
 
-        if ($categorie_elements['id_categorie'] == 2){
-            echo $categorie_elements['article'];
-        }
+   
+} 
+    ?>
 
 
-    
-}?>
-    <a href="?categorie=<?=  1; ?>">cat 1</a>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Articles</title>
+    </head>
+
+    <body>
+        <?php include('elements/header.php'); ?>
+
+
+        <div class="articles">
+            <?php
+            while ($elements =  $req_article->fetch()) { ?>
+
+             
+
+                <h4 id="h4articles"> <?php echo  $elements['login'] . ' ' . ':' . $elements['article'] . ' ' . $elements['date']; ?> </h4> 
+
+                
+                
+
+           <?php } var_dump($elements) ?>
+        </div>
+        
+        <?php
+
+            while($categorie_elements = $req->fetch()){
+
+            if (isset($categorie_elements['id_categorie'])) {
+            echo $elements['login'].':'.' '.$categorie_elements['article'] ;
+            ?> <br> </br> 
+
+    <?php }
+ 
+    if ($categorie_elements['id_categorie'] == 2) {
+        echo $categorie_elements['article'];
+    }
+}
 
 
 
-    <a href="?categorie=<?=  2; ?>">cat 2</a>
 
+        //arrondi à l'entier le plus proche, permert d'avoir touchoir un entier
+        $nbpage = ceil($nbelements / $limite); ?>
 
-    <a href="?categorie=<?=  3; ?>">cat 3</a>
+        <a href="?categorie=<?= 1; ?>">cat 1</a>
+
+        <a href="?categorie=<?= 2; ?>">cat 2</a>
+
+        <a href="?categorie=<?= 3; ?>">cat 3</a>
 
     </div>
-    
+
     <ul class="pagination">
 
 
@@ -109,25 +111,25 @@ $req_article->execute();
         <li class="page-item">
             <?php if ($page > 1) :
             ?> <a href="?page=<?php echo $page - 1 ?>" class="page-link">
-                    </a> <?php
-                        endif;
+                </a> <?php
+                    endif;
 
 
-                            ?>
+                        ?>
 
         </li class="page-item">
 
         <li class="page-item"> <?php
-                                for ($i = 1; $i <= $nbpage; $i++) :
-                                ?><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
-                                                            endfor; ?>
+         for ($i = 1; $i <= $nbpage; $i++) :
+        ?><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a> <?php
+         endfor; ?>
         </li>
 
         <li class="page-item"> <?php
-                                if ($page < $nbpage) :
-                                ?> <a href="?page=<?php echo $page + 1; ?>"> ></a><?php
-                                                        endif;
-                                                            ?>
+        if ($page < $nbpage) :
+         ?> <a href="?page=<?php echo $page + 1; ?>"> ></a><?php
+         endif;
+        ?>
         </li>
 
     </ul>
@@ -136,6 +138,6 @@ $req_article->execute();
 
     <?php include('elements/footer.php'); ?>
 
-</body>
+    </body>
 
-</html>
+    </html>
