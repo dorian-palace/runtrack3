64d5336926ba8249ab3f4b44c5b316ba1d30a4d4
dorian-palace <dorian.palace@laplateforme.io>
2022-03-17 13:21:39 +0100

    job02

--- jour04/job02/scripts.js
+++ jour04/job02/scripts.js
@@ -1,21 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+        const json = {
+            "name": "Laplateforme",
+            "adresse": "8 rue dhozier",
+            "city": "marseille",
+            "nb_staff": "11",
+            "creation": "2019"
+        }
 
-    function jsonValueKey() {
+        var key = 'name'
 
-        const fileJson = new Request('data.json')
+        function JsonValueKey(json, key) {
 
-        fetch(fileJson)
-            .then(response => response.json())
-            .then(data => {
+            console.log(json[key])
 
-                const myJSON = JSON.stringify(data)
-                const city = data.city
-                console.log(city)
-                return document.write(city)
+        }
 
-            })
-            .catch(console.error);
-    }
-    jsonValueKey();
+        JsonValueKey(json, key)
 
-})
+
+        // document.addEventListener("DOMContentLoaded", () => {
+
+        //     function jsonValueKey() {
+
+        //         const fileJson = new Request('data.json')
+
+        //         fetch(fileJson)
+        //             .then(response => response.json())
+        //             .then(data => {
+
+        //                 const myJSON = JSON.stringify(data)
+        //                 const city = data.city
+        //                 console.log(city)
+        //                 return document.write(city)
+
+        //             })
+        //             .catch(console.error);
+        //     }
+        //     jsonValueKey();
+
+        // })
