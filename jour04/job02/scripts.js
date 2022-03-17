7d59d284ec5058f5f85a95b511ebda3fd90fc6cf
dorian-palace <dorian.palace@laplateforme.io>
2022-03-17 12:58:06 +0100

    job2

--- jour04/job02/scripts.js
+++ jour04/job02/scripts.js
@@ -7,14 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(fileJson)
             .then(response => response.json())
             .then(data => {
+
                 const myJSON = JSON.stringify(data)
                 const city = data.city
-                // return myJSON.city
-                // document.getElementById('p').innerHTML = myJSON.city;
-                // console.log(Object.keys(myJSON))
                 console.log(city)
                 return document.write(city)
-             
+
             })
             .catch(console.error);
     }
